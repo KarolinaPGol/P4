b6a3491a36683bed5bc20d0317a3ba57b8797386
KarolinaPGol <31969537+KarolinaPGol@users.noreply.github.com>
2019-05-21 19:24:31 +0200

    with cronbach alfa

--- Evaluation/Evaluation.m
+++ Evaluation/Evaluation.m
@@ -1,24 +1,9 @@
-%[num,txt,raw] = xlsread('myfile') ;
-
 excel_file = readtable('LikertOnly.xlsx');
-
-%thats if we take 2 grups of two scenarios:
-%A = Scenario A - Rick Atley, Fur Elise
-%B = Scenario B - Dr Dre, Poker Face
-% AVisual = table2array(excel_file(1:15,1:12));
-% BVisual = table2array(excel_file(16:30,1:12));
-% 
-% AVibrations = table2array(excel_file(1:15,13:20));
-% BVibrations = table2array(excel_file(16:30,13:20));
-% 
-% AOverall = table2array(excel_file(1:15,21:30));
-% BOverall = table2array(excel_file(16:30,21:30));
 
 % Scenario A & B combined
 Visual = table2array(excel_file(:,1:12));
 Vibrations = table2array(excel_file(:,13:20));
 Overall = table2array(excel_file(:,21:30));
-
 
 % %Mean of every question
 % for i=1:
@@ -32,6 +17,7 @@ Overall = table2array(excel_file(:,21:30));
 %     
 % end
 
+%Histograms for each question and each group
 figure(1)
 for i=1:12
    subplot(2,12,i) %command for making many diagrams subplot(rows,columns,plots)
@@ -60,5 +46,36 @@ for i=1:10
    xlabel(text);
 end
 
+% Cronbachs alfa
+VisualNeg = [1,4,6,8,10,11,12];
+VibrationsNeg = [2,3,5,7];  
+OverallNeg = [1,4,5,8,9,10];
+
+Visual(:,VisualNeg) = 5-Visual(:,VisualNeg); %(all rows, columns negative questions) = and we change them with 5-element value
+Vibrations(:,VibrationsNeg) = 5-Vibrations(:,VibrationsNeg);
+Overall(:,OverallNeg) = 5-Overall(:,OverallNeg);
+
+VisualC_all = cronbach(Visual)
+VibrationsC_all = cronbach(Vibrations)
+OverallC_all = cronbach(Overall)
+
+
+
+%Unused:
+%thats if we take 2 grups of two scenarios:
+%A = Scenario A - Rick Atley, Fur Elise
+%B = Scenario B - Dr Dre, Poker Face
+% AVisual = table2array(excel_file(1:15,1:12));
+% BVisual = table2array(excel_file(16:30,1:12));
+% 
+% AVibrations = table2array(excel_file(1:15,13:20));
+% BVibrations = table2array(excel_file(16:30,13:20));
+% 
+% AOverall = table2array(excel_file(1:15,21:30));
+% BOverall = table2array(excel_file(16:30,21:30));
+
+% VisualNeg = [1,4,6,8,10,11,12];
+% VibrationsNeg = [14,15,17,19];
+% OverallNeg = [21,24,25,28,29,30];
 
 
