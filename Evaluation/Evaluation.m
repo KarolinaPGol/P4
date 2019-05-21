c80d41d5693e233e32c4ea3dcac9ff5abaec076b
KarolinaPGol <31969537+KarolinaPGol@users.noreply.github.com>
2019-05-21 22:51:45 +0200

    Means calculated and other stuff, will continue tomorrow goodnighty

--- Evaluation/Evaluation.m
+++ Evaluation/Evaluation.m
@@ -5,46 +5,35 @@ Visual = table2array(excel_file(:,1:12));
 Vibrations = table2array(excel_file(:,13:20));
 Overall = table2array(excel_file(:,21:30));
 
-% %Mean of every question
-% for i=1:
-%     
-%     r1 = mean(A(:,i)); %mean
-%     sqrt( (sum((A(:,i) - r1(:)).^2)/ m) ); %standard deviation
-%    
-%     subplot
-%     barplot(i);
-%     text
-%     
+
+% %Histograms for each question and each group
+% figure(1)
+% for i=1:12
+%    subplot(2,12,i) %command for making many diagrams subplot(rows,columns,plots)
+%    histogram(Visual(:,i))
+%    textual_number=num2str(i); %converting number into String
+%    text = strcat('Q',textual_number);
+%    xlabel(text);
 % end
-
-%Histograms for each question and each group
-figure(1)
-for i=1:12
-   subplot(2,12,i) %command for making many diagrams subplot(rows,columns,plots)
-   histogram(Visual(:,i))
-   textual_number=num2str(i); %converting number into String
-   text = strcat('Q',textual_number);
-   xlabel(text);
-end
-
-figure(2)
-for i=1:8
-   subplot(1,8,i) %command for making many diagrams subplot(rows,columns,plots)
-   histogram(Vibrations(:,i))
-   textual_number=num2str(i); %converting number into String
-   text = strcat('Q',textual_number);
-   xlabel(text);
-end
-
-
-figure(3)
-for i=1:10
-   subplot(2,10,i) %command for making many diagrams subplot(rows,columns,plots)
-   histogram(Overall(:,i))
-   textual_number=num2str(i); %converting number into String
-   text = strcat('Q',textual_number);
-   xlabel(text);
-end
+% 
+% figure(2)
+% for i=1:8
+%    subplot(1,8,i) %command for making many diagrams subplot(rows,columns,plots)
+%    histogram(Vibrations(:,i))
+%    textual_number=num2str(i); %converting number into String
+%    text = strcat('Q',textual_number);
+%    xlabel(text);
+% end
+% 
+% 
+% figure(3)
+% for i=1:10
+%    subplot(2,10,i) %command for making many diagrams subplot(rows,columns,plots)
+%    histogram(Overall(:,i))
+%    textual_number=num2str(i); %converting number into String
+%    text = strcat('Q',textual_number);
+%    xlabel(text);
+% end
 
 % Cronbachs alfa
 VisualNeg = [1,4,6,8,10,11,12];
@@ -55,9 +44,24 @@ Visual(:,VisualNeg) = 5-Visual(:,VisualNeg); %(all rows, columns negative questi
 Vibrations(:,VibrationsNeg) = 5-Vibrations(:,VibrationsNeg);
 Overall(:,OverallNeg) = 5-Overall(:,OverallNeg);
 
-VisualC_all = cronbach(Visual)
-VibrationsC_all = cronbach(Vibrations)
-OverallC_all = cronbach(Overall)
+VisualC_all = cronbach(Visual);
+VibrationsC_all = cronbach(Vibrations);
+OverallC_all = cronbach(Overall);
+
+All = [Visual Vibrations Overall];
+
+% Means to each question
+EachQuestionMean = mean(All);
+
+VisualMean = mean(Visual,'all');
+VibrationsMean = mean(Vibrations,'all');
+OverallMean = mean(Overall,'all');
+
+AllMean = mean(All, 'all');
+
+%continue with guides from:
+%https://www.statisticssolutions.com/manova-analysis-one-sample-t-test/
+
 
 
 
